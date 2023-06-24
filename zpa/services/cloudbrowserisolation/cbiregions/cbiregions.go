14be0a02d989910ee92bf0b68e2f86867e22434e
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-06-23 21:12:36 -0700

    feat: Added new ZPA CBI endpoints

---
+++ zpa/services/cloudbrowserisolation/cbiregions/cbiregions.go
@@ -0,0 +1,55 @@
+package cbiregions
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/willguibr/zscaler-sdk-go/zpa/services/common"
+)
+
+const (
+	cbiConfig                 = "/cbiconfig/cbi/api/customers/"
+	cbiRegionsEndpoint string = "/regions"
+)
+
+type CBIRegions struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+// The current API does not seem to support search by ID
+func (service *Service) Get(RegionID string) (*CBIRegions, *http.Response, error) {
+	v := new(CBIRegions)
+	relativeURL := fmt.Sprintf("%s/%s", cbiConfig+service.Client.Config.CustomerID+cbiRegionsEndpoint, RegionID)
+	resp, err := service.Client.NewRequestDo("GET", relativeURL, nil, nil, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// The current API does not seem to support search by Name
+func (service *Service) GetByName(cbiRegionName string) (*CBIRegions, *http.Response, error) {
+	relativeURL := cbiConfig + service.Client.Config.CustomerID + cbiRegionsEndpoint
+	list, resp, err := common.GetAllPagesGeneric[CBIRegions](service.Client, relativeURL, "")
+	if err != nil {
+		return nil, nil, err
+	}
+	for _, app := range list {
+		if strings.EqualFold(app.Name, cbiRegionName) {
+			return &app, resp, nil
+		}
+	}
+	return nil, resp, fmt.Errorf("no region named '%s' was found", cbiRegionName)
+}
+
+func (service *Service) GetAll() ([]CBIRegions, *http.Response, error) {
+	relativeURL := cbiConfig + service.Client.Config.CustomerID + cbiRegionsEndpoint
+	list, resp, err := common.GetAllPagesGeneric[CBIRegions](service.Client, relativeURL, "")
+	if err != nil {
+		return nil, nil, err
+	}
+	return list, resp, nil
+}
