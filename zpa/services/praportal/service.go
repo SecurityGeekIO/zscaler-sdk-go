5e0d6bd9b5e425ca538050a5b26ec0d22cea2caa
William Guilherme <wguilherme@securitygeek.io>
2023-07-13 00:10:55 -0700

    feat: Added New ZPA PRA Portal Endpoint

---
+++ zpa/services/praportal/service.go
@@ -0,0 +1,13 @@
+package praportal
+
+import (
+	"github.com/SecurityGeekIO/zscaler-sdk-go/zpa"
+)
+
+type Service struct {
+	Client *zpa.Client
+}
+
+func New(c *zpa.Client) *Service {
+	return &Service{Client: c}
+}
