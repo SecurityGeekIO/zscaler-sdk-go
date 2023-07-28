dc2cb3e9bb2275179e9e1ab168ad1abdb7f23536
William Guilherme <wguilherme@securitygeek.io>
2023-07-28 08:57:57 -0700

    Added SharedMicrotenant option to app segments

--- zpa/services/browseraccess/zpa_browser_access.go
+++ zpa/services/browseraccess/zpa_browser_access.go
@@ -14,36 +14,52 @@ const (
 )
 
 type BrowserAccess struct {
-	ID                        string                `json:"id,omitempty"`
-	SegmentGroupID            string                `json:"segmentGroupId,omitempty"`
-	SegmentGroupName          string                `json:"segmentGroupName,omitempty"`
-	BypassType                string                `json:"bypassType,omitempty"`
-	ConfigSpace               string                `json:"configSpace,omitempty"`
-	DomainNames               []string              `json:"domainNames,omitempty"`
-	Name                      string                `json:"name,omitempty"`
-	Description               string                `json:"description,omitempty"`
-	Enabled                   bool                  `json:"enabled"`
-	PassiveHealthEnabled      bool                  `json:"passiveHealthEnabled"`
-	SelectConnectorCloseToApp bool                  `json:"selectConnectorCloseToApp"`
-	DoubleEncrypt             bool                  `json:"doubleEncrypt"`
-	HealthCheckType           string                `json:"healthCheckType,omitempty"`
-	IsCnameEnabled            bool                  `json:"isCnameEnabled"`
-	IPAnchored                bool                  `json:"ipAnchored"`
-	TCPKeepAlive              string                `json:"tcpKeepAlive,omitempty"`
-	IsIncompleteDRConfig      bool                  `json:"isIncompleteDRConfig"`
-	UseInDrMode               bool                  `json:"useInDrMode"`
-	InspectTrafficWithZia     bool                  `json:"inspectTrafficWithZia"`
-	HealthReporting           string                `json:"healthReporting,omitempty"`
-	ICMPAccessType            string                `json:"icmpAccessType,omitempty"`
-	CreationTime              string                `json:"creationTime,omitempty"`
-	ModifiedBy                string                `json:"modifiedBy,omitempty"`
-	ModifiedTime              string                `json:"modifiedTime,omitempty"`
-	TCPPortRanges             []string              `json:"tcpPortRanges,omitempty"`
-	UDPPortRanges             []string              `json:"udpPortRanges,omitempty"`
-	TCPAppPortRange           []common.NetworkPorts `json:"tcpPortRange,omitempty"`
-	UDPAppPortRange           []common.NetworkPorts `json:"udpPortRange,omitempty"`
-	ClientlessApps            []ClientlessApps      `json:"clientlessApps,omitempty"`
-	AppServerGroups           []AppServerGroups     `json:"serverGroups,omitempty"`
+	ID                        string                   `json:"id,omitempty"`
+	SegmentGroupID            string                   `json:"segmentGroupId,omitempty"`
+	SegmentGroupName          string                   `json:"segmentGroupName,omitempty"`
+	BypassType                string                   `json:"bypassType,omitempty"`
+	ConfigSpace               string                   `json:"configSpace,omitempty"`
+	DomainNames               []string                 `json:"domainNames,omitempty"`
+	Name                      string                   `json:"name,omitempty"`
+	Description               string                   `json:"description,omitempty"`
+	Enabled                   bool                     `json:"enabled"`
+	PassiveHealthEnabled      bool                     `json:"passiveHealthEnabled"`
+	SelectConnectorCloseToApp bool                     `json:"selectConnectorCloseToApp"`
+	DoubleEncrypt             bool                     `json:"doubleEncrypt"`
+	HealthCheckType           string                   `json:"healthCheckType,omitempty"`
+	IsCnameEnabled            bool                     `json:"isCnameEnabled"`
+	IPAnchored                bool                     `json:"ipAnchored"`
+	TCPKeepAlive              string                   `json:"tcpKeepAlive,omitempty"`
+	IsIncompleteDRConfig      bool                     `json:"isIncompleteDRConfig"`
+	UseInDrMode               bool                     `json:"useInDrMode"`
+	InspectTrafficWithZia     bool                     `json:"inspectTrafficWithZia"`
+	HealthReporting           string                   `json:"healthReporting,omitempty"`
+	ICMPAccessType            string                   `json:"icmpAccessType,omitempty"`
+	CreationTime              string                   `json:"creationTime,omitempty"`
+	ModifiedBy                string                   `json:"modifiedBy,omitempty"`
+	ModifiedTime              string                   `json:"modifiedTime,omitempty"`
+	TCPPortRanges             []string                 `json:"tcpPortRanges,omitempty"`
+	UDPPortRanges             []string                 `json:"udpPortRanges,omitempty"`
+	TCPAppPortRange           []common.NetworkPorts    `json:"tcpPortRange,omitempty"`
+	UDPAppPortRange           []common.NetworkPorts    `json:"udpPortRange,omitempty"`
+	ClientlessApps            []ClientlessApps         `json:"clientlessApps,omitempty"`
+	AppServerGroups           []AppServerGroups        `json:"serverGroups,omitempty"`
+	SharedMicrotenantDetails  SharedMicrotenantDetails `json:"sharedMicrotenantDetails,omitempty"`
+}
+
+type SharedMicrotenantDetails struct {
+	SharedFromMicrotenant string `json:"sharedFromMicrotenant,omitempty"`
+	SharedToMicrotenants  string `json:"sharedToMicrotenants,omitempty"`
+}
+
+type SharedFromMicrotenant struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+type SharedToMicrotenant struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 type ClientlessApps struct {
