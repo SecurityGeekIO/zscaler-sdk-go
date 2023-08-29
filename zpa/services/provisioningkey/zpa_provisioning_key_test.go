42581c87ea953557e74929b3f03fa27aaa36640b
William Guilherme <wguilherme@securitygeek.io>
2023-08-28 17:30:19 -0700

    Fix: provisioning key test

--- zpa/services/provisioningkey/zpa_provisioning_key_test.go
+++ zpa/services/provisioningkey/zpa_provisioning_key_test.go
@@ -1,7 +1,6 @@
 package provisioningkey
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -17,15 +16,42 @@ import (
 const connGrpAssociationType = "CONNECTOR_GRP"
 
 // clean all resources
-func init() {
-	log.Printf("init cleaning test")
-	shouldCleanAllResources, _ := strconv.ParseBool(os.Getenv("ZSCALER_SDK_TEST_SWEEP"))
-	if !shouldCleanAllResources {
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
+	cleanResources() // clean up at the beginning
+}
+
+func teardown() {
+	cleanResources() // clean up at the end
+}
+
+func shouldClean() bool {
+	val, present := os.LookupEnv("ZSCALER_SDK_TEST_SWEEP")
+	if !present {
+		return true // default value
+	}
+	shouldClean, err := strconv.ParseBool(val)
+	if err != nil {
+		return true // default to cleaning if the value is not parseable
+	}
+	log.Printf("ZSCALER_SDK_TEST_SWEEP value: %v", shouldClean)
+	return shouldClean
+}
+
+func cleanResources() {
+	if !shouldClean() {
 		return
 	}
+
 	client, err := tests.NewZpaClient()
 	if err != nil {
-		panic(fmt.Sprintf("Error creating client: %v", err))
+		log.Fatalf("Error creating client: %v", err)
 	}
 	service := New(client)
 	resources, _ := service.GetAll()
@@ -33,11 +59,14 @@ func init() {
 		if !strings.HasPrefix(r.Name, "tests-") {
 			continue
 		}
+		log.Printf("Deleting resource with ID: %s, Name: %s", r.ID, r.Name)
 		_, _ = service.Delete(connGrpAssociationType, r.ID)
 	}
 }
 
 func TestProvisiongKey(t *testing.T) {
+	cleanResources()                // At the start of the test
+	defer t.Cleanup(cleanResources) // Will be called at the end
 
 	name := "tests-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 	updateName := "tests-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
