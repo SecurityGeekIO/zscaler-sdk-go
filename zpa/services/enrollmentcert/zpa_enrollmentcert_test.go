c8afb903fe49c4d1e840121407c2a41ac7d2c5ac
William Guilherme <wguilherme@securitygeek.io>
2023-09-11 16:39:31 -0700

    feat: Added zia client ratelimiter and cache functionality

--- zpa/services/enrollmentcert/zpa_enrollmentcert_test.go
+++ zpa/services/enrollmentcert/zpa_enrollmentcert_test.go
@@ -3,7 +3,7 @@ package enrollmentcert
 import (
 	"testing"
 
-	tests "github.com/SecurityGeekIO/zscaler-sdk-go/v2/tests"
+	"github.com/SecurityGeekIO/zscaler-sdk-go/v2/tests"
 )
 
 func TestEnrollmentCert(t *testing.T) {
