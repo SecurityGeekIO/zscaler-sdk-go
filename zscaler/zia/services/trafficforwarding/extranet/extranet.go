f8e46b916c8196247ae0cf0eeedc21f5688d590f
William Guilherme <wguilherme@securitygeek.io>
2026-02-05 21:03:39 -0800

    fix: Sync with main repo

--- zscaler/zia/services/trafficforwarding/extranet/extranet.go
+++ zscaler/zia/services/trafficforwarding/extranet/extranet.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/SecurityGeekIO/zscaler-sdk-go/v3/zscaler"
@@ -28,8 +29,8 @@ type Extranet struct {
 	// Information about the DNS servers specified for the extranet
 	ExtranetDNSList []ExtranetDNSList `json:"extranetDNSList"`
 
-	// Information about the traffic selectors specified for the extranet. Type: Array
-	ExtranetIpPoolList string `json:"extranetIpPoolList"` // Placeholder for "TBD" - refine as needed
+	// Information about the traffic selectors specified for the extranet (API returns an array).
+	ExtranetIpPoolList []ExtranetPoolList `json:"extranetIpPoolList"`
 
 	// The Unix timestamp when the extranet was created
 	CreatedAt int `json:"createdAt"`
@@ -55,6 +56,21 @@ type ExtranetDNSList struct {
 	UseAsDefault bool `json:"useAsDefault"`
 }
 
+type ExtranetPoolList struct {
+	// The ID generated for the DNS server configuration
+	ID int `json:"id"`
+
+	// The name of the DNS server
+	Name string `json:"name"`
+
+	IPStart string `json:"ipStart"`
+
+	IPEnd string `json:"ipEnd"`
+
+	// A Boolean value indicating that the DNS servers specified in the extranet are the designated default servers
+	UseAsDefault bool `json:"useAsDefault"`
+}
+
 func Get(ctx context.Context, service *zscaler.Service, extranetID int) (*Extranet, error) {
 	var extranet Extranet
 	err := service.Client.Read(ctx, fmt.Sprintf("%s/%d", extranetEndpoint, extranetID), &extranet)
@@ -66,15 +82,24 @@ func Get(ctx context.Context, service *zscaler.Service, extranetID int) (*Extran
 	return &extranet, nil
 }
 
+// GetAllOptions holds optional query parameters supported by the extranet API.
+// The API does not support page or pageSize; only orderBy, order, and search are supported.
+type GetAllOptions struct {
+	OrderBy string // e.g. "id", "name"
+	Order   string // e.g. "asc", "desc"
+	Search  string
+}
+
+// GetExtranetByName returns an extranet by name (case-insensitive). It uses GetAll internally
+// so that the non-paginated API is used and page/pageSize are not sent.
 func GetExtranetByName(ctx context.Context, service *zscaler.Service, extranetName string) (*Extranet, error) {
-	var extranets []Extranet
-	err := common.ReadAllPages(ctx, service.Client, extranetEndpoint, &extranets)
+	extranets, err := GetAll(ctx, service, nil)
 	if err != nil {
 		return nil, err
 	}
-	for _, extranet := range extranets {
-		if strings.EqualFold(extranet.Name, extranetName) {
-			return &extranet, nil
+	for i := range extranets {
+		if strings.EqualFold(extranets[i].Name, extranetName) {
+			return &extranets[i], nil
 		}
 	}
 	return nil, fmt.Errorf("no extranet found with name: %s", extranetName)
@@ -114,14 +139,33 @@ func Delete(ctx context.Context, service *zscaler.Service, extranetID int) (*htt
 	return nil, nil
 }
 
-func GetAll(ctx context.Context, service *zscaler.Service) ([]Extranet, error) {
-	var extranet []Extranet
-	err := common.ReadAllPages(ctx, service.Client, extranetEndpoint, &extranet)
-	return extranet, err
+// GetAll returns all extranets. The extranet API does not support pagination (page, pageSize);
+// it only supports orderBy, order, and search. opts may be nil to use no query parameters.
+func GetAll(ctx context.Context, service *zscaler.Service, opts *GetAllOptions) ([]Extranet, error) {
+	endpoint := extranetEndpoint
+	if opts != nil {
+		params := url.Values{}
+		if opts.OrderBy != "" {
+			params.Set("orderBy", opts.OrderBy)
+		}
+		if opts.Order != "" {
+			params.Set("order", opts.Order)
+		}
+		if opts.Search != "" {
+			params.Set("search", opts.Search)
+		}
+		if len(params) > 0 {
+			endpoint = endpoint + "?" + params.Encode()
+		}
+	}
+	var extranets []Extranet
+	err := service.Client.Read(ctx, endpoint, &extranets)
+	return extranets, err
 }
 
+// GetLite returns lite extranet list. Uses Read directly; the extranet API does not support page/pageSize.
 func GetLite(ctx context.Context, service *zscaler.Service) ([]common.IDNameExternalID, error) {
-	var extranet []common.IDNameExternalID
-	err := common.ReadAllPages(ctx, service.Client, fmt.Sprintf("%s/lite", extranetEndpoint), &extranet)
-	return extranet, err
+	var list []common.IDNameExternalID
+	err := service.Client.Read(ctx, fmt.Sprintf("%s/lite", extranetEndpoint), &list)
+	return list, err
 }
